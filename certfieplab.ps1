439cbdfebb17c824c1d12a0f596d3dc43ba1e762
CleberPalhano <138150121+CleberPalhano@users.noreply.github.com>
2023-06-29 15:43:49 -0300

    Update certfieplab.ps1

--- certfieplab.ps1
+++ certfieplab.ps1
@@ -4,6 +4,6 @@ If(!(test-path -PathType container "C:\FIEP"))
       New-Item -ItemType Directory -Path "C:\FIEP"
 }
 #download the cettificate to that folder
-Invoke-WebRequest -Uri https://https://raw.githubusercontent.com/CleberPalhano/certfortinet/main/Certificado%20Rede%20Educacional%20-%20Fortinet.crt -OutFile C:\FIEP\CertFiep.crt;
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/CleberPalhano/certfortinet/main/Certificado%20Rede%20Educacional%20-%20Fortinet.crt -OutFile C:\FIEP\CertFiep.crt;
 #import the certificate into a certificate store
 Import-Certificate -FilePath "C:\FIEP\CertFiep.crt" -CertStoreLocation Cert:\LocalMachine\Root
